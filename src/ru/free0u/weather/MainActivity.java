fda92126accb23bc5aa5bf09fd1b723ed72b89c4
Anton Evdokimov <free0u@gmail.com>
2012-11-18 23:44:49 +0400

    Add many parts of UI

--- src/ru/free0u/weather/MainActivity.java
+++ src/ru/free0u/weather/MainActivity.java
@@ -2,7 +2,10 @@ package ru.free0u.weather;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    
+        LinearLayout lin = (LinearLayout)findViewById(R.id.weatherLayout);
+        LayoutInflater inf = getLayoutInflater();
+        
+
+        View v2 = inf.inflate(R.layout.weather, lin, false);
+        lin.addView(v2);
+        
+        for (int i = 0; i < 3; ++i) {
+        	View v1 = inf.inflate(R.layout.weather_item, lin, false);
+            lin.addView(v1);
+            
+        }
     }
 }
