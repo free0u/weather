8ac681b371d17441275b0ae3c41f8188085397ed
Anton Evdokimov <free0u@gmail.com>
2012-11-18 20:12:30 +0400

    Add Eclipse project

---
+++ src/ru/free0u/weather/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.free0u.weather;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
